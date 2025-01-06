b1f401a6fcf9ebb193a4a19dbfb6617f36d33532
Qaswar Ahmad <100393993+webdeveloperQaswarahmad@users.noreply.github.com>
2025-01-06 12:27:15 +0500

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin:
       process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
+        ? "https://resonant-queijadas-74315c.netlify.app"
         : "https://esaylead-backend-production.up.railway.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
