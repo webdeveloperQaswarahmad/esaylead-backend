bb44cd3cd3f1b11d504a08258c1c9fe3c2e61e98
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2025-01-10 18:04:35 +0500

    cors-issue

--- utils/index.js
+++ utils/index.js
@@ -19,8 +19,9 @@ export const createJWT = (res, userId) => {
   // Change sameSite from strict to none when you deploy your app
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict", //prevent CSRF attack
-    maxAge: 1 * 24 * 60 * 60 * 1000, //1 day
+    secure: process.env.NODE_ENV !== "development", // Set to true for production
+    sameSite: "None", // Allow the cookie to be sent cross-domain
+    maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
+  
 };
