120f72323d15d9381a542f9ea8ec3aad53edf84a
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2025-01-14 11:56:52 +0500

    cors-issues

--- index.js
+++ index.js
@@ -9,18 +9,46 @@ import routes from "./routes/index.js";
 dotenv.config();
 
 dbConnection();
-const router = express.Router();
+// const router = express.Router();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 // Allow specific origins
-app.use(
-  cors({
-    origin: true, // Allow all origins temporarily
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:8800",
+  "https://resonant-queijadas-74315c.netlify.app",
+  "https://esaylead-backend-production.up.railway.app",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    console.log("Origin:", origin);
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error("Not allowed by CORS")); // Reject the request
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"], // Allowed HTTP methods
+  allowedHeaders: [
+    "Content-Type",
+    "Origin",
+    "X-Requested-With",
+    "Accept",
+    "x-client-key",
+    "x-client-token",
+    "x-client-secret",
+    "Authorization",
+  ], // Allowed headers
+  credentials: true, // Allow cookies and authorization headers
+};
+
+app.use(cors(corsOptions));
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: "10mb" })); // Adjust limit as needed
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
--- utils/index.js
+++ utils/index.js
@@ -12,13 +12,14 @@ export const dbConnection = async () => {
 };
 
 export const createJWT = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
+    secure: process.env.NODE_ENV === "production", // Ensures cookies are secure in production
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // SameSite set to "None" in production
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
 };
-
