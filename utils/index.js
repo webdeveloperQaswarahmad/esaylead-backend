51eb38f994a2b3026d15ae49584646af7d27ee70
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2025-01-14 11:26:37 +0500

    cors-issues

--- utils/index.js
+++ utils/index.js
@@ -12,15 +12,13 @@ export const dbConnection = async () => {
 };
 
 export const createJWT = (res, userId) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1d" });
 
-  // Change sameSite from strict to none when you deploy your app
   res.cookie("token", token, {
-    httpOnly: false,
-    secure: process.env.NODE_ENV !== "development", // Set to true for production
-    sameSite: "Strict", // Allow the cookie to be sent cross-domain
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: process.env.NODE_ENV === "development" ? "Lax" : "None",
     maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
   });
 };
+
