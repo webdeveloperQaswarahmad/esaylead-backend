0799d4308dced98cf41864a6fcd899b2d1eda08a
QaswarAhmad <qaswarwebtechahmad@gmail.com>
2025-01-30 18:33:41 +0500

    new cluster created

--- utils/index.js
+++ utils/index.js
@@ -1,20 +1,28 @@
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
+
 export const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = process.env.MONGODB_URI; // Your MongoDB connection string
+
+    if (!uri) {
+      throw new Error("MongoDB URI is not defined!");
+    }
+
+    // Connect to MongoDB using Mongoose
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000, // Timeout after 5 seconds if no server is found
+      serverSelectionTimeoutMS: 10000, // Timeout after 10 seconds if no server is found
       socketTimeoutMS: 45000, // Timeout for socket operations
     });
-    console.log("DB connection established");
+
+    console.log("DB connection established with MongoDB!");
   } catch (error) {
     console.log("DB Error: " + error);
   }
 };
-
 
 export const createJWT = (res, userId) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
